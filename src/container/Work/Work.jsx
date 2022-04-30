6a788173c78d24bbd93cae7d5f2f10a2fea7fd83
Jascuas <loljavierdelago@hotmail.com>
2022-04-30 14:48:13 +0200

    own work

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -48,7 +48,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'Web App', 'Mobile App', 'Blockchain', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}

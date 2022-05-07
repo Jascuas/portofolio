930b1c8ae735dc27646dcf359b3309d56d405ed1
Jascuas <loljavierdelago@hotmail.com>
2022-05-07 17:34:56 +0200

    responsive links

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -20,7 +20,7 @@ const Work = () => {
     client.fetch(skillsQuery).then((data) => {
       setSkills(data);
     });
-    
+
     client.fetch(query).then((data) => {
       setWorks(data);
       setFilterWork(data);
@@ -74,7 +74,7 @@ const Work = () => {
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className="app__work-hover app__flex"
+                className=" app__work-hover"
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
 
@@ -91,6 +91,30 @@ const Work = () => {
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+              <motion.div
+                className=" app__work-mobile"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileTap={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileTap={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >

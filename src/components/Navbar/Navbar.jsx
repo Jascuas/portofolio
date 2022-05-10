2ed070c9206452847ae42aac2238f752d260b583
Jascuas <loljavierdelago@hotmail.com>
2022-05-10 13:25:36 +0200

    link active

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
-
+import { useLocation  } from 'react-router-dom'
+//style={active === item ? { backgroundColor: '#313BAC' } : {}}
 import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [active, setActive] = useState("home");
   const menuItems = ['home', 'about', 'work', 'experiences','skills', 'contact']
+  const location = useLocation();
 
+  useEffect(() => {
+    if(location.hash){
+      setActive(location.hash.substring(1))
+    } else {
+      setActive("home")
+    }
+    console.log(active);
+  }, [location])
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+      <a href={"#home"}><img src={images.logo} alt="logo" /></a>
       </div>
       <ul className="app__navbar-links">
         {menuItems.map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
-            <a href={`#${item}`}>{item}</a>
+            <a href={`#${item}`}
+            style={active === item ? { color: '#313bac' } : {}}
+            >{item}</a>
           </li>
         ))}
       </ul>
@@ -35,7 +48,9 @@ const Navbar = () => {
             <ul>
               {['home', 'about', 'work', 'experiences', 'skills', 'contact'].map((item) => (
                 <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}
+                    style={active === item ? { color: '#313bac' } : {}}
+                  >
                     {item}
                   </a>
                 </li>

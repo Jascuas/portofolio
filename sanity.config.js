5cdf8bec2b15bd444e065e6139273a191f26a967
Jascuas <javier.sanchez@bitnovo.com>
2023-01-29 12:36:28 +0100

    config

---
+++ sanity.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'sanity'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas'
+
+export default defineConfig({
+    title: 'My studio',
+    basePath: '/studio',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: 'production',
+    plugins: [deskTool()],
+    schema: {
+        types: schemaTypes,
+    },
+})
